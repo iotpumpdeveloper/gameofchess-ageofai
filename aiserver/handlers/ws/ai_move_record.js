264350cfc42b234582175aebb1302d888e06c941
jim zike huang <iotpump.developer@gmail.com>
2017-05-08 16:42:57 -0700

    fixed async await call in move recording

--- aiserver/handlers/ws/ai_move_record.js
+++ aiserver/handlers/ws/ai_move_record.js
@@ -2,44 +2,37 @@
  * record the move that ai made for a specific fen
  */
 module.exports = 
-  (context, client) => {
+  async (context, client) => {
     var querystring = require('querystring');
-    var fs = require('fs');
+    var fs = require(__dirname + '/../../libs/FileSystem.js');
     var messageObj = JSON.parse(context.message);
     var fen = messageObj.fen;
     var move = messageObj.move;
     if (typeof fen == 'string' && fen.length > 0) {
       var fenKey = querystring.escape(fen);
       var fenKeyEntry =  context.rootDir + '/../' + context.config.experienceDBDir + '/' + fenKey;
-      fs.access(fenKeyEntry, (err) => {
-        if (!err) { //fen key entry exists, do not record anything
-          client.sendJSON({
+      try {
+        if (await fs.exists(fenKeyEntry)) { //fen key entry exists, do not record anything
+          client.endJSON({
             success : false,
           });
-          client.close();
-        } else { //fen key entry does not exist yet, record it
-          fs.writeFile(fenKeyEntry, JSON.stringify(move), (err) => {
-            if (!err) {
-              //successfully saved
-              console.log('successfully record for fen key entry: ' + fenKeyEntry);
-              client.sendJSON({
-                success : true
-              });
-              client.close();
-            } else {
-              console.log(err);
-              client.sendJSON({
-                success : false
-              }); 
-              client.close();
-            }
-          }); 
+        } else {
+          console.log("recording move for fenKey " + fenKey);
+          await fs.writeFile(fenKeyEntry, JSON.stringify(move)); 
+          context.storage.experience[fenKey] = JSON.stringify(move); //store move in in-memory experience storage
+          client.endJSON({
+            success : true,
+          });
         }
-      });
+      } catch (err) {
+        console.log(err);
+        client.endJSON({
+          success : false,
+        });
+      }
     } else {
-      client.sendJSON({
+      client.endJSON({
         success : false
       }); 
-      client.close();
     }
   }
