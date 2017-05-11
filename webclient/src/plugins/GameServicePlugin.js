09ec7dae216c16b60a3a2e914044329d8819f2b1
jim zike huang <iotpump.developer@gmail.com>
2017-05-11 15:36:54 -0700

    remove unused simple chess ai old library

--- webclient/src/plugins/GameServicePlugin.js
+++ webclient/src/plugins/GameServicePlugin.js
@@ -4,7 +4,6 @@
  */
 import sha1 from 'sha1';
 import Storage from '../libs/Storage.js';
-import SimpleChessAI from '../../../isomorphic/libs/SimpleChessAI.js';
 import Chess from '../../../isomorphic/libs/chess.js';
 
 export default class 
