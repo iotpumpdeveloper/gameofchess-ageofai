b001ca23212cd0da965b9113c83f02acca445c03
jim zike huang <iotpump.developer@gmail.com>
2017-05-15 15:56:29 -0700

    verify the fen string

--- aiserver/cli/search_move.js
+++ aiserver/cli/search_move.js
@@ -1,6 +1,12 @@
 var SimpleChessAI = require(__dirname + '/../libs/SimpleChessAI.js'); 
 
 var args = process.argv;
+
+if (args[2] == undefined) {
+  console.log('Please provide the fen string');
+  process.exit();
+}
+
 var fen = args[2].trim();
 
 var move = SimpleChessAI.getNextBestMove(fen);
