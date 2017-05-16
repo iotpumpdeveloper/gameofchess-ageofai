a27ec557312e48779b89b153121ce78404a3609d
jim zike huang <iotpump.developer@gmail.com>
2017-05-15 22:14:52 -0700

    update test

--- aiserver/test/test_keydistributor.js
+++ aiserver/test/test_keydistributor.js
@@ -1,14 +1,11 @@
-const KeyDistributor = require('../libs/KeyDistributor');
+var KeyDistributor = require('../libs/KeyDistributor.js');
+var Config = require('../libs/Config.js');
+Config.init('../config.json');
 
-var serverMap = {
-  s1 : { weight: 1 },
-  s2 : { weight: 1 },
-  s3 : { weight: 1 }
-}
+var chai = require('chai');  
+var expect = chai.expect;
 
-var kd = new KeyDistributor(serverMap);
-
-var keys = [
+var fenKeys = [
 "r1b1k2r%2Fpppp1ppp%2F3b4%2F2n5%2F8%2FPPq2PP1%2FR1P1KP1P%2F2B2BR1%20b%20kq%20-%200%2012",
 "r1b1k2r%2Fpppp1ppp%2F3b4%2F2n5%2F8%2FPPq2PP1%2FR1P1KP1P%2F2B2BR1%20b%20kq%20-%200%2012",
 "1k1r4%2Fpbpp1pp1%2F6p1%2F1K6%2F5q2%2F8%2Fr7%2F8%20b%20-%20-%201%2030",
@@ -18,8 +15,14 @@ var keys = [
 "r1b1k2r%2Fpppp1pp1%2F8%2F4p2p%2F2P1P2q%2FPPbPK2P%2F3n1N2%2F1RB4R%20b%20kq%20-%200%2022"
 ];
 
-for (var i = 0; i < keys.length; i ++) {
-  var key = keys[i];
-  var serverName = kd.getServerNameForKey(key);
-  console.log(serverName);
+try {
+  var kd = new KeyDistributor();
+
+  for (var i = 0; i < fenKeys.length; i ++) {
+    var key = fenKeys[i];
+    var serverName = kd.getServerNameForKey(key);
+    expect(serverName).to.be.a('string');
+  }
+} catch (err) {
+  console.log(err.message);
 }
