477ec990534deac7a7e45fb5bd57e5bef47cd807
jim zike huang <iotpump.developer@gmail.com>
2017-05-20 11:30:28 -0700

    use the in-project http-server instead of relying on the global one

--- run_demo.sh
+++ run_demo.sh
@@ -4,7 +4,7 @@ cd webclient
 #build bundle javascript first
 webpack -d
 # we should use http-server here for demo
-http-server -p 8080 -s > ../webclient.log 2>&1 &
+node_modules/http-server/bin/http-server -p 8080 -s > ../webclient.log 2>&1 &
 cd ..
 cd aiserver
 node --harmony-async-await server.js s1 > ../aiserver_s1.log 2>&1 &
