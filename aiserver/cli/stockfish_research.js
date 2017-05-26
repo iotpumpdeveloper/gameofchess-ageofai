091b3728dd3f86f5cab104121cf6a9ceea3de568
jim zike huang <iotpump.developer@gmail.com>
2017-05-26 12:43:40 -0700

    use async, await and StockFishChessAI

--- aiserver/cli/stockfish_research.js
+++ aiserver/cli/stockfish_research.js
@@ -1,32 +1,46 @@
-function get_moves()
-{
-  var moves = '';
-  var history = game.history({verbose: true});
-
-  for(var i = 0; i < history.length; ++i) {
-    var move = history[i];
-    moves += ' ' + move.from + move.to + (move.promotion ? move.promotion : '');
-  }
-
-  return moves;
-}
-
+var StockFish = require('stockfish');
 var Chess = require('chess.js').Chess;
 
-var StockFish = require('stockfish');
-
-var stockfish = StockFish();
-
-var game = new Chess();
-
-stockfish.postMessage("uci");
-stockfish.postMessage('position startpos moves' + get_moves());
-stockfish.postMessage("go depth 3");
-
-stockfish.onmessage = function(event) {
-  var data = event.data ? event.data : event;
-  var match = data.match(/^bestmove ([a-h][1-8])([a-h][1-8])([qrbn])?/); //find the best move 
-  if (match !== null && typeof match === 'object') {
-    console.log(match);
+class StockFishChessAI
+{
+  constructor()
+  {
+    this.game = new Chess();
+    this.stockfish = new StockFish();
   }
-};
+
+  _getHistoricalMoves()
+  {
+    var moves = '';
+    var history = this.game.history({verbose: true});
+
+    for(var i = 0; i < history.length; ++i) {
+      var move = history[i];
+      moves += ' ' + move.from + move.to + (move.promotion ? move.promotion : '');
+    }
+
+    return moves;
+  }
+
+  getNextBestMove(depth)
+  {
+    return new Promise( (resolve, reject) => {
+      this.stockfish.onmessage = function(event) {
+        var data = event.data ? event.data : event;
+        var match = data.match(/^bestmove ([a-h][1-8])([a-h][1-8])([qrbn])?/); //find the best move 
+        if (match !== null && typeof match === 'object') {
+          resolve(match);
+        }
+      };
+
+      this.stockfish.postMessage('position startpos moves' + this._getHistoricalMoves());
+      this.stockfish.postMessage("go depth " + depth);
+    });
+  }
+}
+
+var ai = new StockFishChessAI();
+(async () => {
+  var data = await ai.getNextBestMove(3);
+  console.log(data);
+})();
