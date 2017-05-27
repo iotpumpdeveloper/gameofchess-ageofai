afad92b9e760bac6736952258c6191031bcba5ff
jim zike huang <iotpump.developer@gmail.com>
2017-05-27 02:48:30 -0700

    cover all first moves in stockfish ai

--- aiserver/cli/stockfish_self_play.js
+++ aiserver/cli/stockfish_self_play.js
@@ -5,6 +5,22 @@ var querystring = require("querystring");
 var fs = require('fs');
 var experienceDBDir = __dirname + '/../../experience';
 
+var SQUARES = {
+  a8:   0, b8:   1, c8:   2, d8:   3, e8:   4, f8:   5, g8:   6, h8:   7,
+  a7:  16, b7:  17, c7:  18, d7:  19, e7:  20, f7:  21, g7:  22, h7:  23,
+  a6:  32, b6:  33, c6:  34, d6:  35, e6:  36, f6:  37, g6:  38, h6:  39,
+  a5:  48, b5:  49, c5:  50, d5:  51, e5:  52, f5:  53, g5:  54, h5:  55,
+  a4:  64, b4:  65, c4:  66, d4:  67, e4:  68, f4:  69, g4:  70, h4:  71,
+  a3:  80, b3:  81, c3:  82, d3:  83, e3:  84, f3:  85, g3:  86, h3:  87,
+  a2:  96, b2:  97, c2:  98, d2:  99, e2: 100, f2: 101, g2: 102, h2: 103,
+  a1: 112, b1: 113, c1: 114, d1: 115, e1: 116, f1: 117, g1: 118, h1: 119
+};
+
+var SQUARES_MAP = {}
+for( var key in SQUARES ) {
+  SQUARES_MAP[SQUARES[key]] = key;
+}
+
 class StockFishChessAI
 {
   constructor()
@@ -54,14 +70,44 @@ class StockFishChessAI
   }
 }
 
-//play 3000 games
+//cover all possible first movies
 (async () => {
-  for (n = 1; n <= 3000; n++) {
-    console.log("Game Play " + n);
+  var ai = new StockFishChessAI();
+  //get all possible first moves for player 
+  var game = ai.getCurrentGame();
+  var firstMoves = game.ugly_moves();  
+
+  var gameNumber = 0;
+
+  for (mi = 0; mi < firstMoves.length; mi ++) {
+    gameNumber ++;
     var ai = new StockFishChessAI();
-    for (var i = 1; i <= 100; i++) {
+    var game = ai.getCurrentGame();
+    var firstMove = firstMoves[mi];
+    firstMove.color = 'w';
+    firstMove.fromSquare = SQUARES_MAP[firstMove.from];
+    firstMove.toSquare = SQUARES_MAP[firstMove.to];
+    console.log("Game Play " + gameNumber);
+    console.log(1);
+    //player move first, in white 
+    game = ai.getCurrentGame();
+    game.ugly_move(firstMove); 
+
+    var fen = game.fen();
+    var fenKey = querystring.escape(fen);
+    var fenKeyEntry = experienceDBDir + '/' + fenKey;
+
+    //ai move 
+    var move = await ai.getNextBestMove(10);
+    move.color = 'b';
+    move.fromSquare = SQUARES_MAP[firstMove.from];
+    firstMove.toSquare = SQUARES_MAP[firstMove.to];
+
+    fs.writeFileSync(fenKeyEntry, JSON.stringify(move));
+    game.move(move);
+
+    for (var i = 2; i <= 100; i++) {
       console.log(i);
-      var game = ai.getCurrentGame();
 
       if (game.moves().length == 0) {
         console.log('Game over');
@@ -75,6 +121,7 @@ class StockFishChessAI
       //player move first, in white
       var moves = game.ugly_moves(); 
       var move = moves[Math.floor(Math.random() * moves.length)]; //player just do a random move
+      move.color = 'w';
       game.ugly_move(move);
 
       var fen = game.fen();
@@ -83,10 +130,9 @@ class StockFishChessAI
 
       //ai move 
       var move = await ai.getNextBestMove(10);
+      move.color = 'b';
       fs.writeFileSync(fenKeyEntry, JSON.stringify(move));
       game.move(move);
-
-
     }
   }
 })();
