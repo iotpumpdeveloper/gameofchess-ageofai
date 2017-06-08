bbbc0e889339fdcbd507c2d481a0d1af32d5f3c5
jim zike huang <jim.javathunderbird@gmail.com>
2017-06-08 09:03:36 -0700

    start not using require as way to import component, use import instead

--- webclient/src/App.vue
+++ webclient/src/App.vue
@@ -1,16 +1,15 @@
-<template>
-  <div id="main-content">
-    <chess-portal></chess-portal>
-  </div>
+<template src="./App.html">
 </template>
 
 <script>
+  import ChessPortal from './components/ChessPortal/component.vue';
+
   document.title = "Welcome to Game of Chess!";
 
-export default {
-  components : { //register component locally
-    'chess-portal' : require('./components/ChessPortal/component.vue') //one line require local components!!!
+  export default {
+    components : { //register component locally, see document: https://vuejs.org/v2/guide/components.html#Component-Naming-Conventions
+      ChessPortal //by loading component this way, in the template later, we can use tag name like <chess-portal></chess-portal>
+    }
   }
-}
 
   </script>
