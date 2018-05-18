385ab690b3f5089e57a848379190569e7e180130
jim zike huang <jim.javathunderbird@gmail.com>
2018-05-17 23:00:04 -0700

    arrow function refactor

--- webclient/src/main.js
+++ webclient/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueResource from 'vue-resource';
 import WebSocketFactoryPlugin from './plugins/WebSocketFactoryPlugin.js';
-import EventBusPlugin from './plugins/EventBusPlugin.js'; 
+import EventBusPlugin from './plugins/EventBusPlugin.js';
 import GameServicePlugin from './plugins/GameServicePlugin.js';
 
 import $ from 'jquery';
@@ -16,7 +16,7 @@ Vue.use(GameServicePlugin);
 
 new Vue({
   el: '#app',
-  render : function(h) {
+  render : h => {
     return h(App)
   }
 })
