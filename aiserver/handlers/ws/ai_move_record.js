6a7b87e227d513fdfe6fd8c8a2a10036ea94cf3b
jim zike huang <jim.javathunderbird@gmail.com>
2018-07-14 09:49:49 -0700

    add loggin

--- aiserver/handlers/ws/ai_move_record.js
+++ aiserver/handlers/ws/ai_move_record.js
@@ -1,12 +1,13 @@
 /**
  * record the move that ai made for a specific fen
  */
-module.exports = 
+module.exports =
   async (context) => {
     var fenKey = context.client.dk;
     var db = context.db;
     var move = context.client.data.move;
     await db.saveEntry('experience', fenKey, JSON.stringify(move));
+    console.log("saved fenKey " + fenKey + " to server " + context.config.currentServerName);
     return {
       success : true,
     };
